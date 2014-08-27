fe0f9e345717b462da78a755a1146d0688e48ecc
themylogin <themylogin@gmail.com>
2014-08-27 18:32:03 +0700

    More code style

--- tests/datetime/test_timedelta.py
+++ tests/datetime/test_timedelta.py
@@ -1,7 +1,6 @@
-# -*- coding: utf-8 -*-
-from __future__ import absolute_import, unicode_literals
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, division, unicode_literals
 
-from datetime import timedelta
 import unittest
 
 from themyutils.datetime.timedelta import *
