86987231df3416435b5460562f8d4fa9a46cc8d2
themylogin <themylogin@gmail.com>
2014-10-11 13:35:51 +0700

    better retry

--- themyutils/misc.py
+++ themyutils/misc.py
@@ -7,13 +7,21 @@ import time
 
 logger = logging.getLogger(__name__)
 
+__all__ = [b"retry"]
+
 
 def retry(operation, max_tries=100, max_sleep=50, exceptions=(Exception,), logger=logger):
+    exceptions_classes = tuple(exception[0] if isinstance(exception, tuple) else exception
+                               for exception in exceptions)
     tries = 0
     while True:
         try:
             return operation()
-        except exceptions as e:
+        except exceptions_classes as e:
+            for exception_class, is_expected_exception in filter(lambda x: isinstance(x, tuple), exceptions):
+                if isinstance(e, exception_class) and not is_expected_exception(e):
+                    raise
+
             logger.exception(e)
 
             tries += 1
