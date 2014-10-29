08b65241f4bae499335a9c2f9e28e5b97e22f635
themylogin <themylogin@gmail.com>
2014-10-29 20:49:36 +0600

    TerminatableThread => TerminateableThread

--- themyutils/threading.py
+++ themyutils/threading.py
@@ -5,46 +5,46 @@ import ctypes
 import inspect
 import threading
 
-__all__ = [b"TerminatableThread"]
+__all__ = [b"TerminateableThread"]
 
 
 def _async_raise(tid, exctype):
-    """raises the exception, performs cleanup if needed"""
+    # Raises the exception, performs cleanup if needed
     if not inspect.isclass(exctype):
         raise TypeError("Only types can be raised (not instances)")
     res = ctypes.pythonapi.PyThreadState_SetAsyncExc(ctypes.c_long(tid), ctypes.py_object(exctype))
     if res == 0:
-        raise ValueError("invalid thread id")
+        raise ValueError("Invalid thread id")
     elif res != 1:
-        # """if it returns a number greater than one, you're in trouble,
-        # and you should call it again with exc=NULL to revert the effect"""
+        # If it returns a number greater than one, you're in trouble,
+        # and you should call it again with exc=NULL to revert the effect
         ctypes.pythonapi.PyThreadState_SetAsyncExc(tid, 0)
         raise SystemError("PyThreadState_SetAsyncExc failed")
 
 
-class TerminatableThread(threading.Thread):
+class TerminateableThread(threading.Thread):
     def _get_my_tid(self):
-        """determines this (self's) thread id"""
+        # Determines this (self's) thread id
         if not self.isAlive():
-            raise threading.ThreadError("the thread is not active")
+            raise threading.ThreadError("Thread is not active")
 
-        # do we have it cached?
+        # Do we have it cached?
         if hasattr(self, "_thread_id"):
             return self._thread_id
 
-        # no, look for it in the _active dict
+        # No, look for it in the _active dict
         for tid, tobj in threading._active.items():
             if tobj is self:
                 self._thread_id = tid
                 return tid
 
-        raise AssertionError("could not determine the thread's id")
+        raise AssertionError("Could not determine thread's id")
 
     def raise_exc(self, exctype):
-        """raises the given exception type in the context of this thread"""
+        # Raises the given exception type in the context of this thread"""
         _async_raise(self._get_my_tid(), exctype)
 
     def terminate(self):
-        """raises SystemExit in the context of the given thread, which should
-        cause the thread to exit silently (unless caught)"""
+        # Raises SystemExit in the context of the given thread, which should
+        # cause the thread to exit silently (unless caught)
         self.raise_exc(SystemExit)
