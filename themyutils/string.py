a9b740745f7941ce33abac7daa4a6dd0d3095bd7
themylogin <themylogin@gmail.com>
2015-03-05 04:32:20 +0600

    underscore_to_camelcase

--- themyutils/string.py
+++ themyutils/string.py
@@ -2,8 +2,9 @@
 from __future__ import absolute_import, division, unicode_literals
 
 import itertools
+import re
 
-__all__ = [b"common_prefix", b"common_suffix"]
+__all__ = [b"common_prefix", b"common_suffix", b"underscore_to_camelcase"]
 
 
 def common_prefix(strings):
@@ -12,3 +13,13 @@ def common_prefix(strings):
 
 def common_suffix(strings):
     return "".join(reversed(common_prefix(map(reversed, strings))))
+
+
+def underscore_to_camelcase(s, lower_first=True):
+    s = re.sub(r'(?!^)_([a-zA-Z])', lambda m: m.group(1).upper(), s)
+
+    if not lower_first:
+        if len(s):
+            s = s[0].upper() + s[1:]
+
+    return s
