faba4f79f7557c44d1d9131be8566d9196b0049f
themylogin <themylogin@gmail.com>
2015-03-22 18:05:14 +0600

    entity_to_dict

--- themyutils/sqlalchemy/__init__.py
+++ themyutils/sqlalchemy/__init__.py
@@ -1,2 +1,6 @@
 # -*- coding=utf-8 -*-
 from __future__ import absolute_import, division, unicode_literals
+
+
+def entity_to_dict(entity):
+    return {column.name: getattr(entity, column.name) for column in entity.__table__.columns}
