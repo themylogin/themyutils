56aad957ebc646a0634515028cf9c99ea2c4e360
themylogin <themylogin@gmail.com>
2015-03-22 18:05:01 +0600

    itertools.with_prev

--- themyutils/itertools.py
+++ themyutils/itertools.py
@@ -8,3 +8,10 @@ def unique_items(iterable, key=lambda x: x):
         if key(item) not in items:
             items.append(item)
     return items
+
+
+def with_prev(iterable, prev_for_first=None):
+    prev_v = prev_for_first
+    for v in iterable:
+        yield v, prev_v
+        prev_v = v
