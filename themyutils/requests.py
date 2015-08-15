512e1394491666e2434499c9fd5698ff6d9bffe7
themylogin <themylogin@gmail.com>
2015-08-15 23:34:56 +0600

    Google Chrome User-Agent

---
+++ themyutils/requests.py
@@ -0,0 +1,10 @@
+# -*- coding=utf-8 -*-
+from __future__ import absolute_import, division, unicode_literals
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+__all__ = [b"chrome"]
+
+chrome = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2272.76 Safari/537.36"
