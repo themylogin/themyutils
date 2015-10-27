befd9c96a80035a94c73c2d1ebfdad22460f240e
themylogin <themylogin@gmail.com>
2015-10-27 23:24:31 +0600

    Proper celery SQLAlchemy handling

--- themyutils/flask/celery.py
+++ themyutils/flask/celery.py
@@ -6,14 +6,20 @@ from celery import Celery
 __all__ = [b"make_celery"]
 
 
-def make_celery(app):
+def make_celery(app, db=None):
     celery = Celery(app.import_name, broker=app.config["CELERY_BROKER_URL"])
     celery.conf.update(app.config)
-    TaskBase = celery.Task
-    class ContextTask(TaskBase):
+
+    class ContextTask(celery.Task):
         abstract = True
+
         def __call__(self, *args, **kwargs):
             with app.app_context():
-                return TaskBase.__call__(self, *args, **kwargs)
+                return super(ContextTask, self).__call__(self, *args, **kwargs)
+
+        def after_return(self, status, retval, task_id, args, kwargs, einfo):
+            if db:
+                db.session.remove()
+
     celery.Task = ContextTask
     return celery
